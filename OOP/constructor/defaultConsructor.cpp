d2a58b642fc7325b9b65d7104d31980b2a03f55a
Jame <mdjame34@gmail.com>
2025-07-14 05:28:07 +0600

    default constructor

---
+++ OOP/constructor/defaultConsructor.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class somthing_name{
+    public:
+
+    //constructor of the class without any parameters
+    somthing_name(){//constructor name as same as class name
+        cout << "constructor called"<<endl;
+    }
+};
+
+int main(){
+    somthing_name something_object_name;//ekane something_object_name -> something_object_name evabe tkn e likhbo jkn argument pass korbo
+}
