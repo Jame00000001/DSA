403ad10d22a4ba9cf78c6a586c58e85626979a58
Jame <mdjame34@gmail.com>
2025-07-16 18:32:21 +0600

    Traverse of linked list

---
+++ LinkedList/SingularLinkedList/linkedListTraverse.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+   public:
+    int data;
+    Node *next;
+
+    //Creat a constructor ,constructor to set data and next
+    Node(int value){
+        data = value;
+        next = nullptr;
+    }
+};
+
+
+int main() {
+    Node *head,*second,*third;
+    head = new Node(29);
+    second = new Node(20);
+    third = new Node(21);
+    head->next = second ;
+    second->next = third;
+    third->next = nullptr;
+
+    Node *current = head ;
+
+    while (current!=nullptr)
+    {
+        /* code */
+        cout << current->data <<" " ;
+        current = current->next;
+    }
+    
+  return 0;
+    return 0;
+}
